09b93cafcea260d6627c513d04cbd4e1ba81e51a
Angel Penaflor <PenaflorPhi@gmail.com>
2024-08-15 00:17:41 -0600

    Test: Move nvim directory before cloning the repo

--- install.sh
+++ install.sh
@@ -1,27 +1,49 @@
 #!/bin/sh
 
-distro=$(cat /etc/os-release | grep "^NAME=" | cut -d '=' -f 2 | tr -d '"')
-username=$(whoami)
-echo $distro - $username 
+DISTRO=$(cat /etc/os-release | grep "^NAME=" | cut -d '=' -f 2 | tr -d '"')
+USER=$(whoami)
+HOME="/home/$USER/.config"
+CONFIG_DIR="$HOME/.config"
+NEOVIM_DIR="$CONFIG_DIR/nvim"
 
-echo "Upgrading packages and installing dependencies";
-case "$distro" in
-    "Debian GNU/Linux")
-        sudo apt update;
-        sudo apt upgrade;
-        sudo apt install git neovim;
-        ;;
-    "Arch Linux")
-        sudo pacman -Syyyu;
-        sudo pacman -S git neovim;
-        ;;
-    *)
-        echo "IDK unu";
-        ;;
+echo "$DISTRO" - "$USER"
+
+echo "Upgrading packages and installing dependencies"
+case "$DISTRO" in
+"Debian GNU/Linux")
+    sudo apt update
+    sudo apt upgrade
+    sudo apt install git neovim
+    ;;
+"Arch Linux")
+    sudo pacman -Syyyu
+    sudo pacman -S git neovim
+    ;;
+*)
+    echo "IDK unu"
+    ;;
 esac
 
-
 echo "Creating necessary directories and cloning git repo"
-mkdir -p /home/$username/.config/
-cd /home/$username/.config/
+if [ -d "$NEOVIM_DIR" ]; then
+    counter=0
+
+    while true; do
+        if [ "$counter" -eq 0 ]; then
+            NEW_DIR="$CONFIG_DIR/nvim.old"
+        else
+            NEW_DIR="$CONFIG_DIR/nvim.old_$counter"
+        fi
+
+        if [ ! -d "$NEW_DIR" ]; then
+            mv "$NEOVIM_DIR" "$NEW_DIR"
+            echo "Moved neovim configuration to: '$NEW_DIR'"
+            break
+        fi
+        counter=$(expr "$counter" + 1)
+    done
+fi
+
+cd "$CONFIG_DIR" || exit
 git clone https://github.com/PenaflorPhi/nvim
+cd "$NEOVIM_DIR" || exit
