bb1eb9182788ec576fb3b46267c4f209c35df699
Angel Peñaflor <aepenaflor@proton.me>
2025-02-24 01:19:52 -0600

    Added custom configuration to conform for clang-format and sql_formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,48 @@
 return {
-    "stevearc/conform.nvim",
-    config = function () 
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                -- Conform will run multiple formatters sequentially
-                python = { "isort", "black" },
-                -- You can customize some of the format options for the filetype (:help conform.format)
-                rust = { "rustfmt", lsp_format = "fallback" },
-                -- Conform will run the first available formatter
-                javascript = { "prettierd", "prettier", stop_after_first = true },
-            },
-        })
-    end
+	"stevearc/conform.nvim",
+	config = function()
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black", "ruff", "ruff_fix", "ruff_format", "ruff_organize_imports" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				rust = { "rustfmt", lsp_format = "fallback" },
+				-- Conform will run the first available formatter
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				bash = { "beautysh" },
+				go = { "gofmt" },
+				c = { "clang-format" },
+				h = { "clang-format" },
+				cpp = { "clang-format" },
+				hpp = { "clang-format" },
+				sql = { "sql_formatter" },
+				["*"] = { "trim_whitespace", "trim_newlines" },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 1000,
+				lsp_format = "fallback",
+			},
+
+			formatters = {
+				sql_formatter = {
+					args = {
+						"--language=postgresql",
+						'--config={"tabWidth": 4, "keywordCase": "upper"}',
+					},
+				},
+				["clang-format"] = {
+					args = { "-style=file:/home/angel/.config/clang/clang-format" },
+				},
+			},
+		})
+	end,
 }
