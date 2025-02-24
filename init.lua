dbf79eb2530622dc9c448de9ac73eb4a78a13b6a
Angel Peñaflor <aepenaflor@proton.me>
2025-02-23 22:02:50 -0600

    Starting point for new config

--- init.lua
+++ init.lua
@@ -1,10 +1,2 @@
--- 🎀 Load core settings
 require("core.options")
 require("core.keymaps")
-require("core.lazy")
-require("core.autocommands")
-
-require("config.cmp")
-require("config.nvim-lint")
-require("config.mason-lspconfig")
-require("config.lualine")
