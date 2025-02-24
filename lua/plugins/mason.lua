b2a9e4897d9ed63ccc4d2586e7ba6cd7b8647560
Angel Peñaflor <aepenaflor@proton.me>
2025-02-24 01:15:31 -0600

    Changed formatting

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,25 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies={"williamboman/mason-lspconfig.nvim"},
-    config = function()
-        require("mason").setup()
-    end
+	"williamboman/mason.nvim",
+	dependencies = { "williamboman/mason-lspconfig.nvim" },
+	config = function()
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"ruff",
+				"pyright",
+				"bashls",
+				"dockerls",
+				"gopls",
+				"html",
+				"superhtml",
+				"jdtls",
+				"ts_ls",
+				"mdx_analyzer",
+				"sqls",
+				"omnisharp",
+			},
+		})
+	end,
 }
