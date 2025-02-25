efbb5c40686cd3c188a0fda7766a9d21c4787012
Angel Peñaflor <aepenaflor@proton.me>
2025-02-25 00:45:49 -0600

    Added incremental_selection option

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-        local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },  
-        })
-    end
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = { enable = true },
+		})
+	end,
 }
