e3b7a6826493b5f1cdf300e818ae185431affe3f
Angel Peñaflor <aepenaflor@proton.me>
2025-02-25 00:44:36 -0600

    Added function to search symbols in project

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,6 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	vim.keymap.set("n", "<leader>k", require("telescope.builtin").lsp_document_symbols),
 }
