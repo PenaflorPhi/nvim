7b20db0ef500061e5b7ea565360947f427adb761
Angel Peñaflor <aepenaflor@proton.me>
2025-02-25 00:46:19 -0600

    Added a bufferline

---
+++ lua/plugins/bufferline.nvim.lua
@@ -0,0 +1,29 @@
+return {
+	"akinsho/bufferline.nvim",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				diagnostics = "nvim_lsp",
+				show_buffer_close_icons = false,
+				always_show_bufferline = true,
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true, -- use a "true" to enable the default, or set your own character
+					},
+				},
+				highlights = {
+					fill = {
+						bg = {
+							attribute = "fg",
+							highlight = "Pmenu",
+						},
+					},
+				},
+			},
+		})
+	end,
+}
