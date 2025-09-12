e8ff400fab5d478945d139027fcb804e68b5cfbd
Angel Penaflor <aepenaflor@proton.me>
2025-09-12 00:35:01 -0600

    Added formatter for CMake and C/C++

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,9 +6,12 @@ return {
 			-- Conform will run multiple formatters sequentially
 			python = { "isort", "ruff" },
 			-- You can customize some of the format options for the filetype (:help conform.format)
-			rust = { "rustfmt", lsp_format = "fallback" },
+			-- rust = { "rustfmt", lsp_format = "fallback" },
 			-- Conform will run the first available formatter
 			javascript = { "prettierd", "prettier", stop_after_first = true },
+			cmake = { "cmake_format" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
 		},
 		format_on_save = {
 			-- These options will be passed to conform.format()
