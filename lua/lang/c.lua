2f9ec0261e1fc8418f9f62649900a8ca3659ac75
Angel Penaflor <aepenaflor@proton.me>
2025-09-15 02:03:05 -0600

    Added toggle for inlay hints

--- lua/lang/c.lua
+++ lua/lang/c.lua
@@ -2,14 +2,22 @@ local lspconfig = require("lspconfig")
 
 lspconfig.clangd.setup({
 	on_attach = function(client, bufnr)
-		-- Enable inlay hints (Neovim 0.10+)
+		-- Track state
+		local inlay_enabled = true
+
 		if vim.lsp.inlay_hint then
-			vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+			vim.lsp.inlay_hint.enable(inlay_enabled, { bufnr = bufnr })
 		end
 
 		-- Keymaps
 		local opts = { noremap = true, silent = true, buffer = bufnr }
 		vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("v", "<leader>a", vim.lsp.buf.code_action, opts)
+
+		-- Toggle keymap
+		vim.keymap.set("n", "<leader>ih", function()
+			inlay_enabled = not inlay_enabled
+			vim.lsp.inlay_hint.enable(inlay_enabled, { bufnr = bufnr })
+		end, { desc = "Toggle Inlay Hints", buffer = bufnr })
 	end,
 })
