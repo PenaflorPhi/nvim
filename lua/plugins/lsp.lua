22510ec3712d5ff34ce974a2a44a6887c7e2a6a9
Angel Penaflor <aepenaflor@proton.me>
2026-03-22 23:21:09 -0600

    Added pluggin for uv

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,17 @@
--- lsp.lua
 return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = { "mason-org/mason.nvim", "mason-org/mason-lspconfig.nvim" },
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
+			-- C / C++
 			require("lspconfig").clangd.setup({})
 		end,
+	},
+	{
+		"benomahony/uv.nvim",
+		opts = {
+			picker_integration = true,
+		},
 	},
 }
